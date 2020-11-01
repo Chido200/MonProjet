db7a530562145d8bc69b8a4001f7148c38152e82
Chidiac Gaba <chidiac200@gmail.com>
2020-11-01 22:39:42 +0100

    redefinition de models

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Person(models.Model):
     nom = models.CharField(max_length=30)
     prenon = models.CharField(max_length=30, blank=False)
-    /*def __str__(self):
-        return f'{self.nom}  {self.prenon}'*/
+        def __str__(self):
+        return f'{self.nom}  {self.prenon}'
 
 
 
